3e95fb82292e0fa525b69ac2f4986ac8876a671c
algz <26264060@qq.com>
2023-01-10 18:50:05 +0800

    1849

---
+++ algz_eureka_client/src/main/java/com/example/demo/AlgzEurekaClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgzEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgzEurekaClientApplication.class, args);
+	}
+}
